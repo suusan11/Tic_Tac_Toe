9da4acb7f7c5ff234538036139ba14942a61b879
suusan11 <gawow11@gmail.com>
2019-12-05 22:09:50 -0800

    add props

--- src/Board.js
+++ src/Board.js
@@ -2,8 +2,19 @@ import React from 'react';
 import Square from './Square';
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.value}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
